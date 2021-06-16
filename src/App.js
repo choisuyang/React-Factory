3b7f38fe68e83ccd3916a33ba6504a822b508933
choisuyang <chltndid724@gmail.com>
2021-06-16 15:30:47 +0900

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import {createGlobalStyle} from 'styled-components';
-import TodoTemplate from './container/todo/TodoTemplate';
-import TodoHead from './container/todo/TodoHead';
-import TodoList from './container/todo/TodoList';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./container/todo/TodoTemplate";
+import TodoHead from "./container/todo/TodoHead";
+import TodoList from "./container/todo/TodoList";
+import TodoCreate from "./container/todo/TodoCreate";
 
 const GlobalStyle = createGlobalStyle`
   body {
     background: #e9ecef;
   }
-`; 
-
+`;
 
 function App() {
   return (
     <>
-    <GlobalStyle/>
-    <TodoTemplate>
-      <TodoHead/>
-      <TodoList/>
-    </TodoTemplate>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
     </>
   );
 }
