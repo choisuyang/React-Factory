d4d576acb95fd9d9acdb5c41d3fd898f70ebdf06
choisuyang <chltndid724@gmail.com>
2021-07-16 09:55:40 +0900

    setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
-import React, { useState, useRef, useMemo, useCallback } from "react";
-import Counter from "./container/etc/Counter";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/Register";
 
 function App() {
-  return <Counter />;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+
+          <Route exact path="/login" component={LoginPage} />
+
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
