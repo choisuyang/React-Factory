10563c95968532c64e7281026f6560e004c50e3c
choisuyang <chltndid724@gmail.com>
2021-09-07 14:30:22 +0900

    reducer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/Register";
+import axios from "axios";
 
 function App() {
   return (
--- client/src/_reducer/user_reducer.jsx
+++ client/src/_reducer/user_reducer.jsx
@@ -4,10 +4,10 @@ export default function (state = {}, action) {
   switch (action.type) {
     case LOGIN_USER:
       return { ...state, loginSuccess: action.payload };
-      break;
+
     case REGISTER_USER:
       return { ...state, success: action.payload };
-      break;
+
     default:
       return state;
   }
